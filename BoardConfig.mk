8d10cc147d91fc41cfce6a2a7f34d38354ceef41
k4m3l0n <51000885+k4m3l0n@users.noreply.github.com>
2020-04-23 20:41:03 +0200

    BoardConfig: SHRP bringup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -110,4 +110,19 @@ TW_USE_TOOLBOX := true
 TW_HAS_EDL_MODE := true
 PLATFORM_VERSION := 10.0.0
 
-
+#SHRP_Variables
+SHRP_PATH := device/xiaomi/polaris
+SHRP_MAINTAINER := xK4m3l
+SHRP_DEVICE_CODE := Polaris
+SHRP_EDL_MODE := 1
+SHRP_EXTERNAL := /external_sd
+SHRP_INTERNAL := /sdcard
+SHRP_OTG := /usbotg
+SHRP_REC := /dev/block/bootdevice/by-name/recovery
+SHRP_REC_TYPE := Treble
+SHRP_DEVICE_TYPE := A-Only
+SHRP_FLASH := 1
+#SHRP_FONP_1 := /sys/class/leds/led:torch_0/brightness
+#SHRP_FONP_2 := /sys/class/leds/led:torch_1/brightness
+#SHRP_FONP_3 := /sys/class/leds/led:switch/brightness
+#SHRP_FLASH_MAX_BRIGHTNESS := 200
